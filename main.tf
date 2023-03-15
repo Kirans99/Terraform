d6dab7f419c825954c99902b0884dce7356ced7f
kiran kumar reddy <117552103+Kirans99@users.noreply.github.com>
2023-03-15 23:30:51 +0530

    modified file main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,15 @@
 resource "aws_instance" "kk_ec2_instance" {
     ami = "ami-0d81306eddc614a45"
     instance_type = "t2.micro"
+    count = "1"
     tags = {
         Name = "Terraform01"
     }
 } 
+
+resource "aws_s3_bucket" "b2" {
+    bucket = "my-kkrbuckt"
+        tags = {
+        Name = "my-kkrbuckt"
+    }
+}
